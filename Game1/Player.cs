a7e32593dbeed28d66f784babf0fe1dd36fd5afa
Luka98122 <luka98122@gmail.com>
2020-05-28 11:04:41 +0200

    Moved bombing to player

--- Game1/Player.cs
+++ Game1/Player.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
 
 namespace Game1
 {
@@ -81,6 +82,27 @@ namespace Game1
 
             }
 
+        }
+        public void bombing ()
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.A == ButtonState.Pressed && Game1.global.summonCooldown <= 0)
+            {
+                Enemy MyEnemy = new Enemy();
+                Game1.global.ListOfEnemies.Add(MyEnemy);
+                Game1.global.summonCooldown = 20;
+            }
+            if (GamePad.GetState(PlayerIndex.One).Triggers.Right >= 0.7 && bombCount > 0 && Game1.global.bombcooldown < 0)
+            {
+                bombCount--;
+                for (int i = 0; i < Game1.global.ListOfEnemies.Count; i++)
+                {
+                    Game1.global.ListOfEnemies[i].enemyAlive = 0;
+                }
+                
+                Console.Beep(200, 600);
+                Game1.global.bombcooldown = 10;
+            }
+            Game1.global.bombcooldown--;
         }
 
     }
